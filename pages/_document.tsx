731cb3c15e4ee55a0ee886630eecfe71e7f4dfc4
rsrinath1234 <67479260+rsrinath1234@users.noreply.github.com>
2026-03-08 02:32:16 +0530

    favicon logo added
    favicon logo added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+      </Head>
       <body>
         <script
           dangerouslySetInnerHTML={{
